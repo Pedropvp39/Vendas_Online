aabbd8cc3b2575938612d241741af430a9115ea8
Pedro Henrique <ph305099@gmail.com>
2026-08-17 11:11:42 -0300

    Delete Projeto-Integrador directory

--- Projeto-Integrador/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/auth.php';
-
-logout_user();
-
-header('Location: ' . base_url() . '/index.php');
-exit();
-?>
